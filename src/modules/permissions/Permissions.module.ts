9e2af110f502cd90184834d97b188d3f03d5944c
bartik369 <6apmuk@gmail.com>
2025-05-19 17:17:29 +0300

    4

--- src/modules/permissions/Permissions.module.ts
+++ src/modules/permissions/Permissions.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'prisma/prisma.module';
 import { PermissionsController } from './permissions.controller';
 import { PermissionsService } from './permissions.service';
-
+console.log('dsd')
 @Module({
   imports: [PrismaModule],
   controllers: [PermissionsController],
