35bbe85a79efd539292f58cb2cad3be012b43ee9
bartik369 <6apmuk@gmail.com>
2025-05-24 01:25:01 +0300

    333

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,23 +1,26 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor() {
+  constructor(private readonly configService: ConfigService) {
     super({
       datasources: {
         db: {
-          url: process.env.DATABASE_URL,
+          url: configService.get<string>('DATABASE_URL'),
         },
       },
     });
   }
+
   async onModuleInit() {
     await this.$connect();
   }
+
   async onModuleDestroy() {
     await this.$disconnect();
   }
